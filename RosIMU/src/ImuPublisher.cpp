113e66dac3e17c45fcc159d092d1aa804fca881c
Andrew Campbell <arc82@cam.ac.uk>
2017-03-19 18:00:34 +0000

    Changed so that the message is passed as a float. Uses a custom message file

--- RosIMU/src/ImuPublisher.cpp
+++ RosIMU/src/ImuPublisher.cpp
@@ -4,7 +4,7 @@
 #include "sbgCom/sbgCom.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <sstream>
+#include <RosIMU/imu_data.h> //This is the custom message file
 
 //----------------------------------------------------------------------//
 //  Main program                                                        //
@@ -15,17 +15,18 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "ImuPublisher");
 	ros::NodeHandle n;
 	//The ros topic for the imu data is called 'imu_data'
-	ros::Publisher imu_pub = n.advertise<std_msgs::String>("imu_data", 1000);
+	ros::Publisher imu_pub = n.advertise<RosIMU::imu_data>("imu_data", 1000);
 
 
 	SbgProtocolHandle protocolHandle;
 	SbgErrorCode error;
 	SbgOutput output;
+	RosIMU::imu_data msg_packet; 
 
 	//
 	// Init our communications with the device (Please change here the com port and baud rate)
 	//
-	if (sbgComInit("/dev/ttyUSB0", 115200, &protocolHandle) == SBG_NO_ERROR)
+	if (sbgComInit("/dev/ttyUSB1", 115200, &protocolHandle) == SBG_NO_ERROR)
 	{
 		//
 		// Wait until the device has been initialised
@@ -40,7 +41,7 @@ int main(int argc, char** argv)
 		//
 		// Main loop
 		//
-		int test_counter = 0; //This is just a test counter for testing the publishing functionality
+		
 		while (1)
 		{
 			error = sbgGetSpecificOutput(protocolHandle, SBG_OUTPUT_EULER, &output);
@@ -49,16 +50,14 @@ int main(int argc, char** argv)
 				//
 				// Displays sensor values in the console
 				//
-				printf("%3.2f\t%3.2f\t%3.2f\n",	SBG_RAD_TO_DEG(output.stateEuler[0]),
-												SBG_RAD_TO_DEG(output.stateEuler[1]),
-												SBG_RAD_TO_DEG(output.stateEuler[2]));
-				std_msgs::String msg;
-				std::stringstream ss;
-				ss << SBG_RAD_TO_DEG(output.stateEuler[0]) << " " << SBG_RAD_TO_DEG(output.stateEuler[1]) << " " << SBG_RAD_TO_DEG(output.stateEuler[2]) ;
-				//test_counter = test_counter + 1;
-				msg.data = ss.str();
+				printf("%3.2f\t%3.2f\t%3.2f\n",	SBG_RAD_TO_DEG(output.stateEuler[0]), //Roll
+												SBG_RAD_TO_DEG(output.stateEuler[1]), //Pitch
+												SBG_RAD_TO_DEG(output.stateEuler[2])); //Yaw
 				
-				imu_pub.publish(msg);
+				msg_packet.roll = SBG_RAD_TO_DEG(output.stateEuler[0]);
+				msg_packet.pitch = SBG_RAD_TO_DEG(output.stateEuler[1]);
+				msg_packet.yaw = SBG_RAD_TO_DEG(output.stateEuler[2]);
+				imu_pub.publish(msg_packet);
 				ros::spinOnce();
 			}
 
