304fab869eade23811a8f6808674007142df67de
Andrew Campbell <arc82@cam.ac.uk>
2017-03-20 15:46:01 +0000

    Added new package that just reads imu_data meaning you don't need to install the imu software on systems that just want to read the data since you can just not build that package

---
+++ simple_data_reader/src/data_reader.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <msg_definitions/imu_data.h>
+
+void imu_dataCallback(const msg_definitions::imu_data::ConstPtr& msg)
+{	
+	char roll_string[33], pitch_string[10], yaw_string[10];
+	snprintf(roll_string, 10, "%f", msg->roll);
+	snprintf(pitch_string, 10, "%f", msg->pitch);
+	snprintf(yaw_string, 10, "%f", msg->yaw);
+	strcat(roll_string, " ");
+	strcat(roll_string, pitch_string);
+	strcat(roll_string, " ");
+	strcat(roll_string, yaw_string);	
+	ROS_INFO("data_reader read: [%s]", roll_string);	
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "data_reader");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("imu_data", 1000, imu_dataCallback);
+	ROS_INFO("Reader has started");
+	ros::spin();
+	return 0;
+	
+}
